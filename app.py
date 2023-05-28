9612bde3b7096895c956d3941637ad20686bdfda
Maximilien Kpizingui <kpiz1@hotmail.com>
2023-05-28 01:24:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,12 @@ from langchain.callbacks import get_openai_callback
 
 st.set_page_config(page_title="PDF Assistant", page_icon=":robot:")
 st.title("PDF Question Answering App Powered by LLMs")
-st.subheader('AI Web App by [Maximilien](https://kpizmax.hashnode.dev)')
+st.subheader('AI Web App by [Maximilien Kpizingui](https://kpizmax.hashnode.dev)')
+'''
+Say goodbye to manual PDF searching! Our AI-powered app instantly extracts valuable insights from your PDF files, saving you time and effort.With a user-friendly interface, upload your PDF, ask questions, and receive accurate answers in seconds.
+Our intelligent algorithms analyze the text, understand context, and provide precise answers, even for complex queries.Keep track of your queries with session history and easily clear it when needed.
+Experience the power of AI to unlock information from PDFs with our secure and efficient PDF Question Answering App.
+'''
 st.image("post.jpg")
 st.sidebar.markdown("Email: maximilien@tutanota.de")
 st.sidebar.markdown("Element ID: @maximilien:matrix.org")
