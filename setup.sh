0767ec8306bc5f77bda11a7310c11179c750d1f8
Maximilien Kpizingui <kpiz1@hotmail.com>
2023-05-29 09:21:59 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,13 @@
-apt-get install tesseract-ocr
+#!/bin/bash
+
+# Install Tesseract OCR and dependencies
+apt-get install -y tesseract-ocr libtesseract-dev libleptonica-dev
+
+# Install Python packages
+pip install pytesseract==0.3.8 pillow==8.4.0
+
+# Make sure the Tesseract OCR executable is in the PATH
+export TESSDATA_PREFIX=/usr/share/tesseract-ocr/4.00/tessdata/
+
+# Run the Streamlit app
+streamlit run app.py
