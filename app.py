9f73752a6bdba0ff19a9088bf2c538e0dc00664a
Maximilien Kpizingui <kpiz1@hotmail.com>
2024-11-16 14:50:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -66,14 +66,11 @@ def upload_pdf():
             text += page.extract_text()
 
         extracted_text = text
-        st.subheader("PDF Extracted Text Summary")
-        llm = ChatGroq(model="llama-3.1-70b-versatile", groq_api_key=groq_api_key)
-        prompt = SummaryPromptTemplate()
-        chain = LLMChain(llm=llm, prompt=prompt)
-        summary = chain.run({"text": text})
-        st.text(summary)
-        
+        st.subheader("PDF Extracted Text")
+        st.text(extracted_text)
+
         perform_question_answering(extracted_text)
+
 
 
 
